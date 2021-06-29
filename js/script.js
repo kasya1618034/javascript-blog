bf250612be9da22491dbe98840c466d80773ca67
Katarzyna Filas <katarzyna.filas@gmail.com>
2021-06-30 00:25:09 +0200

    Add final titleClickHandler in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
-
+  
   /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -12,24 +13,32 @@ const titleClickHandler = function(event){
 
   /* add class 'active' to the clicked link */
 
-  console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.titles post-title.active');
+  const activeArticles = document.querySelectorAll('article.post');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-}
+  }
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
-  /* add class 'active' to the correct article */
-}
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
+  /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
+}
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
